25b0367234f5bd8430abcce42c084db0b971a3cc
sagocz <58006661+sagocz@users.noreply.github.com>
2025-05-22 22:22:19 +0200

    Development git workflow (#4)
    * add black lint workflow
    
    * lint

--- sensor_simulator/sensor_data.py
+++ sensor_simulator/sensor_data.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class SensorData:
     sensor_id: int
--- sensor_simulator/sensor_node.py
+++ sensor_simulator/sensor_node.py
@@ -7,10 +7,15 @@ from sensor_data import SensorData
 
 BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000/data")
 
+
 def generate_sensor_data(sensor_id: int):
-    return SensorData(sensor_id=sensor_id,timestamp=time.time(),
-                      temperature=round(random.uniform(20.0, 30.0), 2),
-                      pressure=round(random.uniform(100.0, 105.0), 2),)
+    return SensorData(
+        sensor_id=sensor_id,
+        timestamp=time.time(),
+        temperature=round(random.uniform(20.0, 30.0), 2),
+        pressure=round(random.uniform(100.0, 105.0), 2),
+    )
+
 
 def run(sensor_id=1, interval=5):
     while True:
@@ -21,6 +26,7 @@ def run(sensor_id=1, interval=5):
         except Exception as e:
             print(f"Error sending data: {e}")
         time.sleep(interval)
+
 
 if __name__ == "__main__":
     run()
